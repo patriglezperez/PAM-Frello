1bbbffa9e1342925ec764ee68e4c0795c33b40b3
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-02-06 12:08:36 +0100

    Changes of main

--- controllers/lists/getCards.js
+++ controllers/lists/getCards.js
@@ -8,10 +8,6 @@ function getCards(req, res) {
       List = element;
     }
   });
-<<<<<<< HEAD
-  console.log(List)
-=======
->>>>>>> e6a5d4ecd8a28a8e82bbcdff71df2a243bdd5548
   res.status(200).json(List.length > 0 ? List.getCards() : { message: "No cards" });
 }
 
