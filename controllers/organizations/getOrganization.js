f449d881fa003afc20f850a136cc132f6da0b6bf
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-02-06 10:34:45 +0100

    cambiado reduce por filter

--- controllers/organizations/getOrganization.js
+++ controllers/organizations/getOrganization.js
@@ -2,7 +2,7 @@ const context = require('../../models/Context')
 
 function getOrganization(req, res) {
     const contextOrganizations = context.getContextOrganization();
-    const selected = contextOrganizations.reduce(element => element.id === req.params.id)
+    const selected = contextOrganizations.filter(element => element.id === req.params.id)
     res.status(200).json(selected)
 }
 
