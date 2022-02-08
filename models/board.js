460aad06f360890df643c42570b9679ac7b3c5b2
aldo-castellano <92597205+aldo-castellano@users.noreply.github.com>
2022-02-08 21:35:26 +0100

    create board

--- models/board.js
+++ models/board.js
@@ -7,6 +7,7 @@ class board {
       (this.name = name),
       (this.IDorganization = idOraganization),
       (this.Background = "#00000"),
+      (this.idLists = []),
       (this.BackgroundColor = "#00000"),
       (this.Url = "/PENE");
   }
